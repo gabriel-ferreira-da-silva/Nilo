407986f69fb3f3ba3ee10174b042f280872f5297
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2024-09-08 15:18:39 -0300

    stash cash

--- db/nilodb_insert_product.sql
+++ db/nilodb_insert_product.sql
@@ -1,29 +1,132 @@
 INSERT INTO product (name, description, category, image_url, rate, price ) 
 VALUES ('Laptop', 'High-performance laptop with 16GB RAM', 'Electronics', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtkzYKocfGN_vdeGOWuVB7eUDRYP6oTJX3gQ&s', 4.5, 100.22);
 
-INSERT INTO product (name, description, category, image_url, rate, price) 
-VALUES ('Smartphone', 'Latest smartphone with 5G capability', 'Electronics', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHypEHKjydK679slVDWaV_IP1AWiTMskttnA&s',1.22,4.8);
+INSERT INTO product (name, description, category, image_url, rate, price ) 
+VALUES ('smartphone aest', ' veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. ', 'Electronics', 'https://i.pinimg.com/originals/39/0d/2f/390d2f36930f4b9a79360522c1a15900.jpg', 4, 1000);
 
-INSERT INTO product (name, description, category, image_url, rate, price) 
-VALUES ('Desk Chair', 'Ergonomic chair for comfortable seating', 'Furniture', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHypEHKjydK679slVDWaV_IP1AWiTMskttnA&s', 1.22,431.2);
+INSERT INTO product (name, description, category, image_url, rate, price ) 
+VALUES ('book aest', ' veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. ', 'Electronics', 'https://wallpapers.com/images/hd/book-aesthetic-pictures-0oyawsjlb7b2z5ze.jpg', 3, 40);
 
-INSERT INTO product (name, description, category, image_url, rate, price) 
-VALUES ('Running Shoes', 'Lightweight shoes for long-distance running', 'Sportswear', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxpVnViRu-EmQY7GNmWa8Wk_C0Q9YbGNEXaw&s', 4.7, 4.7);
+INSERT INTO product (name, description, category, image_url, rate, price ) 
+VALUES ('aquecedor', 'Product photography can increase your e-commerce sales. The more appealing your products look, the more customers it attracts. One of the reasons why photography is so vital is because people aren’t very good at explaining things they see. porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. ','casa e lar', 'https://uxmag.com/wp-content/uploads/files/Uxmag-articles/2020-Articles/AestheticEffect/AestheticEffect2.png', 3.5, 32);
 
-INSERT INTO product (name, description, category, image_url, rate, price) 
-VALUES ('Watch', 'Smartwatch with heart rate monitoring', 'Accessories', 'https://anatomich.com/wp-content/uploads/2018/02/womens-watch-cinnamon-1.jpg', 4.4,  314.7);
+INSERT INTO product (name, description, category, image_url, rate, price ) 
+VALUES ('produto de beleza', ' veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. ','produto de beleza','https://i.pinimg.com/originals/d6/45/c4/d645c4a49e74b5d2f0b09e71f142aaaf.jpg', 1.5, 55);
 
-INSERT INTO product (name, description, category, image_url, rate, price) 
-VALUES ('cama box', 'cama box para toda a familia', 'casa', 'https://panoverse-cdn.com.br/img.panorama/produto/58179/base-de-cama-box-casal-monaco-cinamomo-tcil-moveis-500469-large.jpg', 1.4,  300.7);
+INSERT INTO product (name, description, category, image_url, rate, price ) 
+VALUES ('set computador', ' veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. ', 'eletronico','https://www.threekit.com/hubfs/_img/Example%20Screens/luggage-screenshot_example-small.jpg', 4.6, 5500);
 
-INSERT INTO product (name, description, category, image_url, rate, price) 
-VALUES ('foguete', 'foguete para toda a familia', 'armas e aerospaco', 'https://img.odcdn.com.br/wp-content/uploads/2023/01/Destaque-foguete.jpg', 1.4,  11111000000.7);
+INSERT INTO product (name, description, category, image_url, rate, price ) 
+VALUES ('componentes desk computador', ' veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. ','eletronico', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrZPx_qopj1jb4GYJ3DKSI1Bh1LCsPysaoDMtqXhlWatt5mCoIX06lX7fKpgfsA3DCkv0&usqp=CAU', 3.8, 690);
 
-INSERT INTO product (name, description, category, image_url, rate, price) 
-VALUES ('bola', 'bola de futebol classica para jogar com seus amingos', 'armas e aerospaco', 'https://images.tcdn.com.br/img/img_prod/646374/bola_de_futebol_juvenil_n_5_271_1_20201213220437.jpg', 2.4,  10);
+INSERT INTO product (name, description, category, image_url, rate, price ) 
+VALUES ('relogio tech', ' veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. ','eletronico', 'https://bewatchpersonalizado.com.br/wp-content/uploads/2022/04/digital-led-furta-cor.png', 3.8, 690);
 
-INSERT INTO product (name, description, category, image_url, rate, price) 
-VALUES ('bandeira', 'bola de futebol classica para jogar com seus amingos', 'armas e aerospaco', 'https://images-americanas.b2w.io/produtos/1821768295/imagens/bandeira-do-para-1-45m-x-1m/1821768308_1_xlarge.jpg', 2.4,  2.10);
 
-INSERT INTO product (name, description, category, image_url, rate, price) 
-VALUES ('camisa', 'bola de futebol classica para jogar com seus amingos', 'armas e aerospaco', 'https://images.tcdn.com.br/img/img_prod/275189/camisa_azul_royal_100_poliester_para_sublimacao_m_2701_1_20200722153204.jpg', 2.4,  2.10);
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES 
+('Relógio Tech', 'Veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem.', 'eletrônico', 'https://bewatchpersonalizado.com.br/wp-content/uploads/2022/04/digital-led-furta-cor.png', 3.8, 690);
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Smartphone X', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.', 'eletrônico', 'https://images.tcdn.com.br/img/img_prod/606732/produto_teste_nao_comprar_2533_1_b9b99499b285edc7dce881e3e8fb098a_20240828145943.png', 4.5, 1200);
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES('gamecube', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.', 'eletrônico', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Gamecube-console.jpg/1024px-Gamecube-console.jpg', 4.5, 1200);
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES('Fones Bluetooth Y', 'Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et vulputate.', 'acessório', 'https://cdn.awsli.com.br/600x600/2707/2707753/produto/267105945/produto-teste-apenas-amostra-5054c785-ui2mug4cxw.png', 4.2, 300),
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES('Antena my tech', 'Phasellus imperdiet, nulla et dignissim bibendum, dui purus dignissim urna, sed lacinia erat magna ut ante.', 'comunication', 'https://upload.wikimedia.org/wikipedia/commons/5/56/WildBlueDish.jpg', 4.9, 2500);
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Laptop UltraSlim', 'Suspendisse potenti. In euismod nisl eu metus fermentum, nec feugiat lacus ornare.', 'informática', 'https://cdn.awsli.com.br/600x600/2707/2707753/produto/267105945/produto-teste-apenas-amostra-89f6ec10-qn2u7dut7n.png', 3.6, 3200);
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Tablet Pro', 'Aenean bibendum urna id neque pharetra, nec lacinia purus consequat. Cras euismod felis sit amet.', 'eletrônico', 'https://lojapapelex.vtexassets.com/arquivos/ids/247850/img-produto-teste--1-.png?v=638442279283370000', 4.0, 950);
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Teclado Mecânico', 'Aliquam erat volutpat. Integer vel ligula consequat, vestibulum elit et, bibendum nunc.', 'acessório', 'https://images.tcdn.com.br/img/img_prod/606732/produto_teste_nao_comprar_2533_1_b9b99499b285edc7dce881e3e8fb098a_20240828145943.png', 3.3, 150);
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES('gamecube', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.', 'eletrônico', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Gamecube-console.jpg/1024px-Gamecube-console.jpg', 4.5, 1200);
+
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES('Mouse Gamer', 'Vivamus euismod lacinia nulla id interdum. Morbi ac massa facilisis, imperdiet dui nec, consectetur erat.', 'acessório', 'https://cdn.awsli.com.br/600x600/2707/2707753/produto/267105945/produto-teste-apenas-amostra-5054c785-ui2mug4cxw.png', 4.4, 200);
+
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES('Monitor 4K', 'Sed pulvinar massa in arcu vestibulum, eu interdum odio vehicula. Donec gravida lorem vel orci.', 'eletrônico', 'https://cdn.awsli.com.br/600x600/2707/2707753/produto/267105945/produto-teste-apenas-amostra-89f6ec10-qn2u7dut7n.png', 4.6, 1800);
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Roteador WiFi', 'Aliquam tincidunt diam at eros auctor, sit amet malesuada risus tristique. Aenean a magna non nisi.', 'eletrônico', 'https://images.tcdn.com.br/img/img_prod/606732/kit_teste_2515_1_a54bab0298356a66aa94c7d7b027314b_20240828145930.png', 4.1, 600);
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Cadeira Ergonômica', 'Curabitur sit amet libero nec quam tempus posuere et a ante. Donec condimentum libero sit amet.', 'mobiliário', 'https://images.tcdn.com.br/img/img_prod/606732/produto_teste_nao_comprar_2533_1_b9b99499b285edc7dce881e3e8fb098a_20240828145943.png', 4.7, 800);
+
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Mesa de Escritório', 'Vestibulum viverra erat eu diam suscipit, sed tristique lacus eleifend. Morbi id fringilla nunc.', 'mobiliário', 'https://cdn.awsli.com.br/600x600/2707/2707753/produto/267105945/produto-teste-apenas-amostra-5054c785-ui2mug4cxw.png', 3.9, 1200);
+
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Micro-ondas', 'Nullam aliquet tortor in eros fermentum, nec cursus nisi dignissim. Curabitur ac mauris varius.', 'eletrodoméstico', 'https://cdn.awsli.com.br/600x600/2707/2707753/produto/267105945/produto-teste-apenas-amostra-89f6ec10-qn2u7dut7n.png', 3.7, 500);
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Aspirador de Pó', 'Mauris dictum elit vel arcu blandit, nec ultricies felis vulputate. Fusce ac tortor interdum.', 'eletrodoméstico', 'https://images.tcdn.com.br/img/img_prod/606732/kit_teste_2515_1_a54bab0298356a66aa94c7d7b027314b_20240828145930.png', 4.3, 750);
+
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Geladeira', 'Pellentesque sit amet orci id dolor eleifend tristique et ac quam. Nullam nec metus ut.', 'eletrodoméstico', 'https://images.tcdn.com.br/img/img_prod/606732/produto_teste_nao_comprar_2533_1_b9b99499b285edc7dce881e3e8fb098a_20240828145943.png', 4.8, 2000);
+
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Fogão', 'Sed consequat purus at urna convallis, sed scelerisque nunc gravida. Aliquam tincidunt erat.', 'eletrodoméstico', 'https://cdn.awsli.com.br/600x600/2707/2707753/produto/267105945/produto-teste-apenas-amostra-5054c785-ui2mug4cxw.png', 3.5, 1000);
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Lavadora de Roupas', 'Ut vulputate dolor non felis hendrerit, ac malesuada augue aliquet. Donec convallis pretium.', 'eletrodoméstico', 'https://cdn.awsli.com.br/600x600/2707/2707753/produto/267105945/produto-teste-apenas-amostra-89f6ec10-qn2u7dut7n.png', 4.0, 1400);
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Ventilador', 'Duis vehicula odio sit amet dictum fringilla. Vestibulum ut est interdum, convallis nisi vitae.', 'eletrodoméstico', 'https://images.tcdn.com.br/img/img_prod/606732/kit_teste_2515_1_a54bab0298356a66aa94c7d7b027314b_20240828145930.png', 3.2, 250);
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Aquecedor', 'Integer gravida mauris a augue venenatis, a scelerisque libero tincidunt. Cras laoreet felis.', 'eletrodoméstico', 'https://cdn.awsli.com.br/600x600/2707/2707753/produto/267105945/produto-teste-apenas-amostra-5054c785-ui2mug4cxw.png', 4.4, 400);
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Blender', 'Curabitur dictum elit nec sapien vehicula, eu faucibus nunc posuere. Pellentesque nec sapien.', 'eletrodoméstico', 'https://images.tcdn.com.br/img/img_prod/606732/produto_teste_nao_comprar_2533_1_b9b99499b285edc7dce881e3e8fb098a_20240828145943.png', 4.1, 300);
+
+
+INSERT INTO product (name, description, category, image_url, rate, price)
+VALUES
+('Espresso', 'Sed consequat mi sit amet urna tincidunt, at scelerisque elit aliquet. Vivamus vitae erat.', 'eletrodoméstico', 'https://cdn.awsli.com.br/600x600/2707/2707753/produto/267105945/produto-teste-apenas-amostra-89f6ec10-qn2u7dut7n.png', 4.6, 500);
+
+
