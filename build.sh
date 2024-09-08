785fc0b06ea5d5e3943997043711955356126f2e
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2024-09-08 16:23:38 -0300

    feat: added shell file for building and running aplication

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Setting up the database..."
+cd db || exit
+mysql -u NILOuser -pNILOuser NILODB < nilodb_create.sql
+mysql -u NILOuser -pNILOuser NILODB < nilodb_insert.sql
+cd ..
+
+echo "Starting backend service..."
+cd backend || exit
+npm install
+cd server || exit
+node ProductServer.js &
+cd ../..
+
+echo "Starting frontend service..."
+cd frontend || exit
+npm install
+npm start
+
+echo "Backend and frontend services are running."
